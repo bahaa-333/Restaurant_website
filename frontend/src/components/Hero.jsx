6f8aea3eb9cf91ba1bdcc45da4bbf0f93f9f9ca1
bahaa-333 <113023302+bahaa-333@users.noreply.github.com>
2026-01-06 22:54:40 +0200

    hero section implementation

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "../assets/logo_no_bg.png";
+import plate from "../assets/burger_no_bg.png";
+import video from "../assets/grill_video.mp4";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import SwiperShow from "./SwiperShow.jsx";
+
+const Hero = () => {
+  return (
+    <div className="hero-section">
+      <div className="w-3/4 h-dvh">
+        {/* Left Side - video */}
+        {/*stack of video, logo, slogan*/}
+        <div className="relative w-full h-full overflow-hidden">
+          <video
+            src={video}
+            muted
+            autoPlay
+            loop
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 flex flex-col items-start justify-end gap-4 p-8 pointer-events-none">
+            <img
+              src={logo}
+              alt="Plan-B logo"
+              className="w-[26vw]  drop-shadow-xl"
+            />
+            <h2 className="text-white text-2xl font-montserrat leading-snug drop-shadow-lg pl-4">
+              the perfect Plan-B for when Mom's homecooked meals <br /> just
+              won't cut it
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-1/4 h-dvh bg-[#1c1c1c]">
+        {/* Right Side - verical caroussel  */}
+        <SwiperShow />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ frontend/src/components/SwiperShow.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import swiper1 from "../assets/caroussel_imgs/swiper1.jpeg";
+import swiper2 from "../assets/caroussel_imgs/swiper2.jpeg";
+import swiper3 from "../assets/caroussel_imgs/swiper3.jpeg";
+import swiper4 from "../assets/caroussel_imgs/swiper4.jpeg";
+import swiper5 from "../assets/caroussel_imgs/swiper5.jpeg";
+
+const SwiperShow = () => {
+  return (
+    <Swiper
+      modules={[Autoplay]}
+      direction="vertical"
+      slidesPerView={2}
+      centeredSlides
+      spaceBetween={18}
+      loop
+      speed={3000}
+      allowTouchMove={false}
+      autoplay={{
+        delay: 0,
+        disableOnInteraction: false,
+        reverseDirection: true,
+      }}
+      className="h-full w-full py-4"
+    >
+      {[swiper1, swiper2, swiper3, swiper4, swiper5].map((img, index) => (
+        <SwiperSlide
+          key={index}
+          className="aspect-square flex items-center justify-center"
+        >
+          <img
+            src={img}
+            alt={`Slide ${index + 1}`}
+            className="h-full w-full object-contain rounded-lg"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default SwiperShow;
