6ffa07f3408ab3c1fc2afd912dd468392fa78971
bahaa-333 <113023302+bahaa-333@users.noreply.github.com>
2026-01-31 18:33:41 +0200

    custom button added transition

--- frontend/src/modules/CustomButton.jsx
+++ frontend/src/modules/CustomButton.jsx
@@ -7,7 +7,15 @@ const CustomButton = ({ props }) => {
       className="relative w-60 h-14 focus:outline-none group"
     >
       {/* Bottom layer */}
-      <div className="absolute inset-0 bg-[#FFD746] translate-y-2 translate-x-2" />
+      <div
+        className="
+      absolute inset-0 
+      bg-[#FFD746] 
+      translate-y-2 translate-x-2 
+      group-hover:shadow-2xl 
+      group-hover:drop-shadow-black
+      "
+      />
 
       {/* Top button */}
       <div
@@ -19,6 +27,7 @@ const CustomButton = ({ props }) => {
           font-montserrat font-bold uppercase
           transition-all duration-200
           group-hover:-translate-y-0.5
+          group-hover:shadow-lg group-hover:drop-shadow-[rgba(1,1,1,0.75)]
           group-active:translate-y-1
         "
       >
