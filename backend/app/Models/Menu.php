ab20183f36d33bbdcbf2eeb33e0ca9a4825f4a34
Bahaa Zaakie <113023302+bahaa-333@users.noreply.github.com>
2026-02-23 01:22:45 +0200

    databse + cloud + authentication setup

--- backend/app/Models/Menu.php
+++ backend/app/Models/Menu.php
@@ -7,6 +7,6 @@ use MongoDB\Laravel\Eloquent\Model;
 class MenuItem extends Model {
     protected $connection = 'mongodb';
     protected $collection = 'menu_items';
-    protected $fillable = ['name', 'description', 'price', 'category', 'image_url'];
+    protected $fillable = ['name', 'description', 'price', 'category', 'active', 'bestseller', 'image_url'];
 }
 
--- backend/config/database.php
+++ backend/config/database.php
@@ -31,9 +31,15 @@ return [
 
     'connections' => [
         'mongodb' => [
-            'driver' => 'mongodb',
-            'dsn' => env('DB_URI'),
-            'database' => env('DB_DATABASE')
+            'driver'   => 'mongodb',
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'restaurant_db'),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'options'  => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+            ],
         ],
 
         'sqlite' => [
